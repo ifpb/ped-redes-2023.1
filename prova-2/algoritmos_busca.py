c992f34fcceb1b17d0ecd80aa13228b391ad531c
Diego Pessoa <diego.pessoa@ifpb.edu.br>
2023-06-12 19:20:30 -0300

    resposta da prática daa prova 2

--- prova-2/algoritmos_busca.py
+++ prova-2/algoritmos_busca.py
@@ -2,17 +2,17 @@ def busca_binaria_recursiva(lista, arquivo):
     if lista is None or len(lista) == 0:
         return -1
     meio = len(lista) // 2
-    if lista[meio].carga.tamanho == arquivo.tamanho:
+    if lista[meio].carga.nome == arquivo.nome:
         return lista[meio].carga.tamanho
-    elif lista[meio].carga.tamanho < arquivo.tamanho:
+    elif lista[meio].carga.nomenome < arquivo.tamanho:
         return busca_binaria_recursiva(lista[meio+1:], arquivo)
     else:
         return busca_binaria_recursiva(lista[:meio], arquivo)
 
 def busca_sequencial_recursiva(lista, arquivo, i=0):
-    if i == len(lista) or lista[i].carga.tamanho > arquivo.tamanho:
+    if i == len(lista) or lista[i].carga.nome > arquivo.nome:
         return -1
-    if lista[i].carga.tamanho == arquivo.tamanho:
+    if lista[i].carga.nome == arquivo.nome:
         return lista[i].carga.tamanho
     return busca_sequencial_recursiva(lista, arquivo, i+1)
 
